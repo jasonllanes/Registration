027656a437afe79dff45e64fe2eb32c331b043aa
Jason <llanezjaizon@gmail.com>
2023-03-28 20:38:08 +0300

    Suicidal Commit

--- app/src/main/java/com/example/registration/Registration_Activity.java
+++ app/src/main/java/com/example/registration/Registration_Activity.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registration_Activity extends AppCompatActivity {
 
-    EditText etFullname,etEmail,etPassword;
+    EditText etFirstName,etLastName,etEmail,etPassword,etConfirmPassword;
     Button btnCreate;
 
     public static final String EXTRA_MESSAGE = "com.example.registration.MESSAGE";
@@ -20,9 +21,11 @@ public class Registration_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etFullname = findViewById(R.id.etFullname);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
 
         btnCreate = findViewById(R.id.btnCreate);
 
@@ -32,15 +35,23 @@ public class Registration_Activity extends AppCompatActivity {
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullname = etFullname.getText().toString();
+                String firstName = etFirstName.getText().toString();
+                String lastName = etLastName.getText().toString();
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
+                String confirmPassword = etConfirmPassword.getText().toString();
 
-                Intent intent = new Intent(Registration_Activity.this,Welcome_Activity.class);
-                intent.putExtra(EXTRA_MESSAGE,"Fullname: " + fullname + "\n" +
-                        "Email: " + email + "\n" +
-                        "Password: " + password + "\n");
-                startActivity(intent);
+                if(confirmPassword.equalsIgnoreCase(password)){
+                    Intent intent = new Intent(Registration_Activity.this,Welcome_Activity.class);
+                    intent.putExtra(EXTRA_MESSAGE,"Fullname: " + firstName + " " + lastName + "\n" +
+                            "Email: " + email + "\n" +
+                            "Password: " + password + "\n");
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(Registration_Activity.this, "Password doesn't match!", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
